62ea3f92460a401f33b66a73d88bdd40cb6e68c8
Throre <pawtet100500@gmail.com>
2022-07-01 17:11:45 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,30 @@
+
+$('.main-slider').slick({
+    infinite: true,
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+          }
+        }
+      ]
+  });
+
+const aside = document.getElementById("aside")
+
+function openAside(){
+    aside.classList.add("active")
+}
+function closeAside(){
+    aside.classList.remove("active")
+}
